e2dc3ed647e6e8bfc9db32eced9b140d2765b23b
Ian Hansson <ian.hansson@gmail.com>
2019-12-01 10:41:03 +0000

    adding notes about scene and camera

--- src/testing.ts
+++ src/testing.ts
@@ -3,7 +3,10 @@
 const THREE = <any>(<any>window).THREE;
 
 const scene = new THREE.Scene();
-/* Scenes allow you to set up what and where is to be rendered by three.js. This is where you place objects, lights and cameras.
+/* 
+
+Scenes allow you to set up what and where is to be rendered by three.js. This is where you place objects, lights and cameras.
+
 Properties
 	- Fog : Fog
 	- overrideMAterial : Material (sets material on everything)
@@ -12,9 +15,46 @@ Properties
 Methods
 	- toJSON : creates json object of scene metadata
 	- dispose : clears data
+
 */
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+/*
+
+This projection mode is designed to mimic the way the human eye sees. It is the most common projection mode used for rendering a 3D scene.
+
+Constructor
+	- Fov
+	- Aspect ratio
+	- Near distance
+	- Far distance
+
+Frustum is a cone like geometry used for defining the visible field.
+
+Properties
+	- Aspect
+	- Far
+	- filmGauge : default 35mm
+	- filmOffset
+	- focus
+	- fov
+	- isPerspectiveCamera
+	- near
+	- view
+	- zoom
+
+Methods
+	- clearViewOffset 
+	- getEffectiveFOV : Gets FOV considering the zoom
+	- getFilmHeight
+	- getFilmWidth
+	- getFocalLength
+	- setFocalLength
+	- setViewOffset
+	- updateProjectionMatrix : Must be called after changes of parameters
+	- toJSON : creates json object of metadata
+
+*/
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
